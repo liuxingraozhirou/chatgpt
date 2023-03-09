feecd3c58d89c42c36be90e9c2c79f13eb879546
流星绕指柔 <876426359@qq.com>
2023-03-09 21:53:43 +0800

    Add files via upload

--- src/default.ts
+++ src/default.ts
@@ -10,7 +10,7 @@ export const defaultMessage = `
 
 - 特感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
 
-- 本站由 [流星绕指柔](https://mp.weixin.qq.com/s/HTYv9FxUAJDzh0yWLFNotQ) 搭建。
+- 本站由 [流星绕指柔](https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIxNDU2NTQ0NA==&scene=124#wechat_redirect) 搭建。
 
 - 想要部署的朋友，可关注公众号：**java空间**，添加我个人微信获取保姆级部署文档，
 
